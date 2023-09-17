3ab301a3d6323ea6cc53ffd96c9d529eebabacfc
cmwema <calebmwema65@gmail.com>
2023-09-17 14:24:34 +0300

    initial commit

---
+++ base/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.home, name='home'),
+    path('room/', views.room, name='room'),
+    path('room/<int:pk>', views.room, name='room'),
+    path('create-room/', views.create_room, name='create_room'),
+    path('update-room/<int:pk>', views.update_room, name='update_room'),
+    path('delete-room/<int:pk>', views.delete_room, name='delete_room'),
+]
