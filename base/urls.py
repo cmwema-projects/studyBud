17b1b9a718515c4ced5fae09682e7d1b95f35162
cmwema <calebmwema65@gmail.com>
2023-09-18 11:02:49 +0300

    Ui update(btn on hover and nav bar)

--- base/urls.py
+++ base/urls.py
@@ -2,6 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
+    path('login/', views.login_view, name='login'),
     path('', views.home, name='home'),
     path('room/', views.room, name='room'),
     path('room/<int:pk>', views.room, name='room'),
