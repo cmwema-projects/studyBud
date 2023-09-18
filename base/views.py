281cac511824b52b7115e1940039962d89541c5d
cmwema <calebmwema65@gmail.com>
2023-09-18 11:32:27 +0300

    added User log in feature

--- base/views.py
+++ base/views.py
@@ -1,10 +1,32 @@
 from django.shortcuts import render, redirect
 from django.db.models import Q
+from django.contrib.auth.models import User
+from django.contrib.auth import authenticate, login, logout
+from django.contrib import messages
+
 from .models import Room, Topic
 from .forms import RoomForm
 
 
 def login_view(request):
+    if request.method == 'POST':
+        username = request.POST.get('username')
+        password = request.POST.get('password')
+
+        # try:
+        #     user = User.objects.get(username=username)
+
+        # except:
+        #     messages.error(request, 'User does not exist')
+
+        user = authenticate(request, username=username, password=password)
+
+        if user is not None:
+            login(request, user)
+            messages.success(request, 'User successfully logged in.')
+            return redirect('home')
+        else:
+            messages.error(request, 'Username or password does not exist.')
     context = {}
 
     return render(request, 'base/login_register.html', context)
